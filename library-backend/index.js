2494148fda02310316ddb4d22f489931fe505dbb
Jolene T <kartistenn2@gmail.com>
2026-09-23 05:18:23 +0000

    added bookCount and authorCount resolvers, Author and Book typedefs

--- library-backend/index.js
+++ library-backend/index.js
@@ -97,15 +97,31 @@ let books = [
   you can remove the placeholder query once your first one has been implemented 
 */
 
-const typeDefs = `
+const typeDefs = /* GraphQL */`
+  type Author {
+    name: String!
+    born: String
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: String!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   },
 }
 
