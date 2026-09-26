caadd69554165484f5a6d41d7ac396580ada9ad0
Jolene T <kartistenn2@gmail.com>
2026-09-26 11:59:25 +0000

    added mutation to add books

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client/react'
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,10 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  const [addBook] = useMutation(ADD_BOOK, {
+      refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+      onError: (error) => console.log(error)
+    })
 
   if (!props.show) {
     return null
@@ -13,8 +19,13 @@ const NewBook = (props) => {
 
   const submit = async (event) => {
     event.preventDefault()
-
-    console.log('add book...')
+    addBook({ variables: {
+        title,
+        author,
+        published: Number(published),
+        genres
+      }
+    })
 
     setTitle('')
     setPublished('')
